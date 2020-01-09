432880f49e1948637edf038922f4335007626f0b
Gabriel Llamas <g.llamas@safened.com>
2020-01-09 21:54:37 +0100

    better styles organization

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,14 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const {
-  CleanWebpackPlugin
-} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: [path.resolve(__dirname, 'src/index.ts')],
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
   },
   resolve: {
     extensions: ['.js', '.ts', 'scss']
@@ -29,30 +27,26 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: 'styles.css',
-      chunkFilename: '[id].css',
+      chunkFilename: '[id].css'
     }),
     new webpack.HotModuleReplacementPlugin()
   ],
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/
       },
-      /*{
-           test: /\.scss$/,
-           use: [{
-             loader: MiniCssExtractPlugin.loader,
-             options: {
-               esModule: true,
-               hmr: isDevEnvironment(),
-             },
-           }, 'sass-loader'],
-         }*/
       {
         test: /\.s(a|c)ss$/,
-        loader: [
-          MiniCssExtractPlugin.loader,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: true
+            }
+          },
           'css-loader',
           {
             loader: 'sass-loader',
